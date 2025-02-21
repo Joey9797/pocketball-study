bd2cbef42c9bc9d60bdd1d621bc55cf493f0682b
juhee park <pjhch1009@gmail.com>
2025-02-21 17:54:53 +0900

    250221 test 02

--- test1.py
+++ test1.py
@@ -41,18 +41,28 @@ def down_left(wx, wy, x, y):  # 목적구가 왼쪽 아래에 있을 때
     return 180 + result
 
 
-# ball_data = list(map(int, input().split("/")))
-ball_data = list(map(int, input().split("/")))
-print(ball_data)
+HOLES = [[0 ,0], [127,0], [254, 0], [0, 127], [127, 127], [254, 127]]
+ball_data = [float(x) for x in input().split('/') if x]
+wx, wy = ball_data[0], ball_data[1]
+for i in range(2, len(ball_data), 2):
+    if ball_data[i] > 0:
+        x, y = ball_data[i], ball_data[i+1]
+        break
 
-# if y > wy and x > wx:  # 목적구가 오른쪽 위에 있을 때
-#     print(up_right(wx, wy, x, y))
+# hole 확인하기
 
-# elif wy > y and x > wx:  # 목적구가 오른쪽 아래에 있을 때
-#     print(down_right(wx, wy, x, y))
 
-# elif y > wy and wx > x:  # 목적구가 왼쪽 위에 있을 때
-#     print(up_left(wx, wy, x, y))
+# 스트레이트샷
+if y > wy and x > wx:  # 목적구가 오른쪽 위에 있을 때
+    print(up_right(wx, wy, x, y))
 
-# elif wy > y and x > wx:  # 목적구가 왼쪽 아래에 있을 때
-#     print(up_left(wx, wy, x, y))
+elif wy > y and x > wx:  # 목적구가 오른쪽 아래에 있을 때
+    print(down_right(wx, wy, x, y))
+
+elif y > wy and wx > x:  # 목적구가 왼쪽 위에 있을 때
+    print(up_left(wx, wy, x, y))
+
+elif wy > y and wx > x:  # 목적구가 왼쪽 아래에 있을 때
+    print(down_left(wx, wy, x, y))
+
+# 빗겨치기
--- test2.py
+++ test2.py
@@ -1,26 +1,31 @@
-WIDTH = 254
-HEIGHT = 127
-RADIUS = 5.73 / 2  # 공의 반지름
+import math
 
-HOLES = [[0, 0], [127, 0], [254, 0], [0, 127], [127, 127], [254, 127]]
-# [x, y]
-NUMBER_OF_BALLS = 6
-is_first = True  # 내가 선플레이어
-balls = [[0, 0] for i in range(NUMBER_OF_BALLS)]
-server_str = "64/64/250/122/-1/-1/-1/-1/-1/-1/-1/-1/"
-data = server_str[: len(server_str) - 1].split("/")
-data = [int(n) for n in data]
-print(data)
-idx = 0
-for i in range(NUMBER_OF_BALLS):
-    for j in range(2):
-        balls[i][j] = data[idx]
-        idx += 1
-print(balls)
+# 스트레이트샷
+def straight(wx, wy, x, y):
+    a = y - wy
+    b = x - wx
+    r = math.sqrt(a**2 + b**2)
 
-if is_first:
-    # 1, 3, 5 처리
-    pass
-else:
-    # 2, 4, 5 처리
-    pass
+    radian = math.atan2(b, a)
+    result = math.degrees(radian)
+    if result < 0:
+        result += 360
+    return result
+
+
+# hole 확인하기
+
+# 빗겨치기
+
+
+HOLES = [[0 ,0], [127,0], [254, 0], [0, 127], [127, 127], [254, 127]]
+ball_data = [float(x) for x in input().split('/') if x]
+wx, wy = ball_data[0], ball_data[1]
+for i in range(2, len(ball_data), 2):
+    if ball_data[i] > 0:
+        x, y = ball_data[i], ball_data[i+1]
+        break
+
+print(straight(wx, wy, x, y))
+
+
